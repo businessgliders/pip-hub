9ea045727547244bafb81bb964f4b9ff7dce7bf0
Pilates in Pink Studio <info@pilatesinpinkstudio.com>
2026-05-19 16:21:40 +0000

    File changes

--- src/pages/AppHub.jsx
+++ src/pages/AppHub.jsx
@@ -1087,7 +1087,7 @@ export default function AppHub() {
       )}
 
       {/* End Shift floating button + modal (owner only) */}
-      {user?.email === 'info@pilatesinpinkstudio.com' && !showCustomizePanel && (
+      {user?.email === 'info@pilatesinpinkstudio.com' && !showCustomizePanel && !showMoreSheet && (
         <>
           <EndShiftButton onClick={() => setShowEndShift(true)} />
           {showEndShift && (
